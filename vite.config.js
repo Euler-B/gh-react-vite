a36e6601c8c24bb922bc7d7ff848c98db9979e74
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-05-08 19:25:03 -0400

    FIX Bug 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://ncc-1701-v3.github.io/',
+  base: 'https://ncc-1701-v3.github.io/gh-react-vite/',
   plugins: [react()]
 })
